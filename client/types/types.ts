1cb2760ce96f4a3c7ef0540f53894a3b2a931e71
Shahab Ali <sali.bese20seecs@seecs.edu.pk>
2024-12-17 18:47:27 +0500

    Feature(Auth): Frontend and backend portion of the authentication completed

---
+++ client/types/types.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    bio: string | null;
+    profile_picture: string | null;
+    created_at: string;
+    updated_at: string;
+  }
+  
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,41 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { CustomLoggerService } from './common/logger/custom-logger.service';
+
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost', 
-    port: 5433,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE, 
-    synchronize: true, 
-  })
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true
+      }
+    ),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 5434),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    AuthModule
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService,{
+    provide: 'Logger',
+    useClass: CustomLoggerService,  // Use the custom logger
+  },],
 })
 export class AppModule {}
